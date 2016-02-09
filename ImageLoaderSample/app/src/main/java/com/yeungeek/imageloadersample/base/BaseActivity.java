c51b7328965f7b31e15d9f16aa2c7ec5274450cb
yeungeek <yangjian0410@gmail.com>
2016-02-09 20:44:37 +0800

    init imageloader smaple v1

---
+++ ImageLoaderSample/app/src/main/java/com/yeungeek/imageloadersample/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.yeungeek.imageloadersample.base;
+
+import android.app.Activity;
+
+/**
+ * Created by yeungeek on 2016/2/16.
+ */
+public abstract class BaseActivity extends Activity{
+}
