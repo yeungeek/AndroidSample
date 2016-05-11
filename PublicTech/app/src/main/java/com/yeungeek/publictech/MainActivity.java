59d7cc3751ef7e8004987ec3c268ac8f70f84ce5
yeungeek <yangjian0410@gmail.com>
2016-05-11 23:17:47 +0800

    user butterknife

--- PublicTech/app/src/main/java/com/yeungeek/publictech/MainActivity.java
+++ PublicTech/app/src/main/java/com/yeungeek/publictech/MainActivity.java
@@ -2,19 +2,27 @@ package com.yeungeek.publictech;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTest;
+    @BindView(R.id.id_annotation)
+    Button mAnnotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTest = (TextView) findViewById(R.id.id_test);
+        ButterKnife.bind(this);
+    }
 
-        mTest.setText(new SimplePojo("s1,", "s2").toString());
+    @OnClick(R.id.id_annotation)
+    public void onAnnotation(Button btn) {
+        btn.setText(new SimplePojo("s1,", "s2").toString());
     }
 }
